159229319414be916493d0b1e25008e0d74b61ad
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 21:45:07 +0530

    Update 10.c

--- OOPS_LAB/LAB_1/10.c
+++ OOPS_LAB/LAB_1/10.c
@@ -1,21 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> //included library for standarad input and output
 
+//define the required structure for storing date
 struct Date{
     int day;
     int month;
     int year;
 };
 
+//define the required structure for storing student details
 struct Student{
     int roll;
     char name[100];
     struct Date dob;
 };
 
-void main()
+int main()
 {
-    struct Student s;
+    struct Student s; // Declare a variable s of type student
 
+    // Input values from the user and store them in structure member
     printf("Enter Student details:\n");
     printf("Roll Number: ");
     scanf("%d", &s.roll);
@@ -29,8 +32,10 @@ void main()
     printf("Year: ");
     scanf("%d", &s.dob.year);
 
+    // Display the stored student details
     printf("\nStudent Information:\n");
     printf("Roll Number: %d\n", s.roll);
     printf("Name: %s\n", s.name);
     printf("Date of Birth: %d/%d/%d\n",s.dob.day, s.dob.month, s.dob.year);
+    return 0;
 }
