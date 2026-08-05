d016b57c88648975d81c2661bc3a819ca2e6eb4c
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 21:59:49 +0530

    Update 6.c

--- OOPS_LAB/LAB_1/6.c
+++ OOPS_LAB/LAB_1/6.c
@@ -1,24 +1,28 @@
-#include<stdio.h>
+#include<stdio.h> //included library for standarad input and output
 
+//define the required structure
 typedef struct{
    float inch ;
    float feet ;
 }distance;
 
 void main(){
-    distance d1,d2;
+    distance d1,d2;   // Declare two variable d1 and d2 of type distant
 
+    // Input values from the user and store them in structure member
     printf("Enter first distance:\n");
     printf("Feet:");
     scanf("%f",&d1.feet);
     printf("Inches : ");
     scanf("%f",&d1.inch);
 
+    
     printf("Enter second distance:\n");
     printf("Feet:");
     scanf("%f",&d2.feet);
     printf("Inches : ");
     scanf("%f",&d2.inch);
 
+    // Display the stored student details
     printf("\nTotal Distance = %f Feet %f Inches\n", d1.feet + d2.feet, d1.inch + d2.inch);
 }
