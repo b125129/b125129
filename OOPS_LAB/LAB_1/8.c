aabb1f265e8f0d3499df7de13f08f04016f533b0
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 22:09:10 +0530

    Update 8.c

--- OOPS_LAB/LAB_1/8.c
+++ OOPS_LAB/LAB_1/8.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>    //included library for standarad input and output
 
+//define the required structure
 typedef struct{
     int empID;
     char name[100];
     float salary;
 }Employee;
 
-void main(){
-    Employee employee[5];
-    int i;
+int main(){
+    Employee employee[5];    //Declare an array of structure to store multiple employee data    
+    int i; //loop counter
+    
+    //loop for storing data of multiple employees.
     for(i = 0; i < 5; i++){
         printf("\nEnter details of Employees %d:\n", i + 1);
         printf("Employee ID: ");
@@ -19,6 +22,7 @@ void main(){
         scanf("%f", &employee[i].salary);
     }
 
+    // for loop to check for employee with maximum salary
     int count,max_salary=employee[0].salary;
     for (int i = 0; i < 5; i++)
     {
@@ -28,7 +32,8 @@ void main(){
             count=i;
         } 
     }
-    
+
+    // printing details of the employee with maximum salary
     printf("\nDetails of Employee with maximum salary:\n");
     printf("Employee ID: %d\n", employee[count].empID);
     printf("Salary: %.2f\n", employee[count].salary);
