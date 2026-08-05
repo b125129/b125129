3a9b2cadb8f4e21ca38aec55e8a8bb5ae5697d7b
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 22:13:14 +0530

    Update 9.c

--- OOPS_LAB/LAB_1/9.c
+++ OOPS_LAB/LAB_1/9.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include<stdio.h>    //included library for standarad input and output
 
-typedef struct {
+typedef struct {    //define the required structure
     int roll;
     char name[100];
     float cgpa;
 }student;
 
-void main(){
-    student s[5];
+int main(){
+    student s[5]; //defining a array of structure student
+    // taking input of data from the user
     printf("Enter roll, name and cgpa of the student:\n");
     for (int i = 0; i < 5; i++)
     {
@@ -17,6 +18,7 @@ void main(){
         scanf("%f",&s[i].cgpa);
     }
 
+    // Display the stored student details who meets the criteria
     printf("Details of students with CGPA greater than or equal to 8.0 are as follows");
 
     for (int i = 0; i < 5; i++)
@@ -26,4 +28,5 @@ void main(){
             printf("\n Roll No: %d\n Name:%s \n CGPA: %.2f \n\n" ,s[i].roll , s[i].name ,s[i].cgpa);
         }
     }
+    return 0;
 }
