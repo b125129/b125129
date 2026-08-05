06ae2742c7e58f37c095a8a75bc7b536ee869f77
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 21:56:53 +0530

    Update 5.c

--- OOPS_LAB/LAB_1/5.c
+++ OOPS_LAB/LAB_1/5.c
@@ -1,17 +1,22 @@
-#include<stdio.h>
+#include<stdio.h> //included library for standarad input and output
 
+//define the required structure
 typedef struct{
    float length ;
    float breadth ;
 }rectangle;
 
-void main(){
-    rectangle r1;
-    printf("Enter Length : ");
-    scanf("%f",&r1.length);
-    printf("Enter Breadth : ");
-    scanf("%f",&r1.breadth);
+int main(){
+   rectangle r1; // Declare a variable r1 of type rectangle
+   
+   // Input values from the user and store them in structure member
+   printf("Enter Length : ");
+   scanf("%f",&r1.length);
+   printf("Enter Breadth : ");
+   scanf("%f",&r1.breadth);
 
-    printf("The Area of Rectangle:%f\n",r1.breadth*r1.length);
-    printf("The perimeter of Rectangle:%f",2*(r1.breadth+r1.length));
+   // Display the stored student details
+   printf("The Area of Rectangle:%f\n",r1.breadth*r1.length);
+   printf("The perimeter of Rectangle:%f",2*(r1.breadth+r1.length));
+   return 0;
 }
