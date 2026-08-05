ec45e15364f66194fa350d716c41af8707d54951
b125129 <b125129@iiit-bh.ac.in>
2026-08-05 21:54:51 +0530

    Update 4.c

--- OOPS_LAB/LAB_1/4.c
+++ OOPS_LAB/LAB_1/4.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
+#include<stdio.h> //included library for standarad input and output
 
+//define the required structure
 typedef struct{
     int proid;
     char name[50];
@@ -7,8 +8,9 @@ typedef struct{
     int quantity;
 } product;
 
-void main(){
-    product p;
+int main(){
+    product p; // Declare a variable p of type product
+    // Input values from the user and store them in structure member
     printf("Enter product Id : ");
     scanf("%d",&p.proid);
     printf("Enter name: "); 
@@ -17,5 +19,7 @@ void main(){
     scanf("%f",&p.price);
     printf("Enter quantity: ");
     scanf("%d",&p.quantity);
+    // calculating the total cost and displaying output
     printf("The Total Cost is %.2f" ,p.price*p.quantity);
+    return 0;
 }
