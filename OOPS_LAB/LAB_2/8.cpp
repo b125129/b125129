c589fe9ed518864d94f8b81f509714db9117dc49
b125129 <b125129@iiit-bh.ac.in>
2026-08-09 07:41:09 +0530

    Update 8.cpp

--- OOPS_LAB/LAB_2/8.cpp
+++ OOPS_LAB/LAB_2/8.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 using namespace std;
 
-
+// Class definition to manage library book issuance and fine calculations
 class Book{
+    // Private data members
     int book_id ;
     char book_title[50];
     char student_name[50];
     int no_of_days ;
 
     public:
+    // Function to take borrowing details as input from the user
     void get_data(){
         cout <<"Enter Book Id: " ;
         cin >> book_id;
@@ -19,6 +21,7 @@ class Book{
         cout <<"Enter No of Days:";
         cin >> no_of_days ;
     }
+    // Function to display student borrowing details
     void student_details(){
         cout<<"\n-------Student details-------" ;
         cout <<"\nBook Id = "<<book_id;
@@ -26,7 +29,7 @@ class Book{
         cout <<"\nStudent Name = "<<student_name ;
         cout <<"\nNo of Days = "<<no_of_days ;
     }
-
+    // Function to calculate and display fine based on total days kept
     void fine(){
         int fine_amount ;
         if(no_of_days>15){
@@ -36,6 +39,7 @@ class Book{
             cout <<"\nNo Fine Amount" ;
         }
     }
+    // Helper function to trigger both transaction details and fine display
     void Display_trascation(){
         student_details();
         fine();
@@ -44,7 +48,8 @@ class Book{
 };
 
 int main(){
-    Book b1;
+    Book b1;    // Instantiate an object 'b1' of class 'Book'
+    
     b1.get_data();
     b1.Display_trascation();
     return 0;
