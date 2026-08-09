ec516d3243edcd7f2d4f509fcb6f62f679d701a7
b125129 <b125129@iiit-bh.ac.in>
2026-08-09 07:27:49 +0530

    Update 4.cpp

--- OOPS_LAB/LAB_2/4.cpp
+++ OOPS_LAB/LAB_2/4.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
+// Class definition to model a Bank Account entity
 class Account{
+    // Private data members
     int acc_num ;
     char name[50];
     float balance ;
 
     public:
+      // Function to gather account information from the user
       void get_detail(){
         cout <<"Enter Account Number: " ;
         cin >> acc_num;
@@ -15,6 +18,7 @@ class Account{
         cout <<"Enter balance:";
         cin >> balance ;
       }
+      // Function to process a deposit transaction
       void Deposit(){
         int dep_amm ;
         cout <<"\nAmount deposited:";
@@ -25,6 +29,7 @@ class Account{
 
         cout <<"\nAmount After deposition = "<<balance;
       }
+      // Function to process a withdrawal transaction
       void Withdraw(){
         int dr_amm ;
         cout <<"\nEnter amount to withdraw:";
@@ -35,21 +40,20 @@ class Account{
 
          cout <<"\nTotal amount After Withdraw = "<< balance ;
       }
-
+      // Function to print updated account details
       void updated(){
         cout<<"\n-------Account details-------" ;
         cout <<"\nAccount Number = "<<acc_num;
         cout <<"\nHolder name ="<<name;
         cout <<"\nBalance = "<<balance ;
       }
-
-
 };
 
 
 int main(){
-    Account a1;
+    Account a1;    // Instantiate an 'Account' object named 'a1'
 
+    // Sequentially invoke class methods
     a1.get_detail();
     a1.Deposit();
     a1.Withdraw();
