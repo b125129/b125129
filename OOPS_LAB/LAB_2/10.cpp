32a66dd0a0c7db55e08eb5d45ab3ff740d3fa655
b125129 <b125129@iiit-bh.ac.in>
2026-08-09 07:44:18 +0530

    Update 10.cpp

--- OOPS_LAB/LAB_2/10.cpp
+++ OOPS_LAB/LAB_2/10.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// Class definition to calculate electricity bills based on slab rates
 class ElectricityBill {
 private:
+    // Private data members
     int con_num;
     char con_name[50];
     int units;
     int total;
 public:
+    // Function to collect consumer information and consumption details
     void details(){
         cout << "Enter Consumer Number: ";
         cin >> con_num;
@@ -16,6 +19,7 @@ public:
         cout << "Enter Units Consumed: ";
         cin >> units;
     }
+    // Function to compute the bill total using tiered slab rates
     void calc_bill(){
     if (units <= 100) {
         total = units * 5;
@@ -27,6 +31,7 @@ public:
         total = (100 * 5) + (100 * 7) + ((units - 200) * 10);
     }
     };
+    // Function to print the complete electricity bill details
     void display(){
         cout << "\nConsumer Number: " << con_num;
         cout << "\nConsumer Name: " << con_name;
@@ -36,7 +41,9 @@ public:
 };
 
 int main() {
+    // Create an instance (object) of ElectricityBill
     ElectricityBill bill;
+    
     bill.details();
     bill.calc_bill();
     bill.display();
