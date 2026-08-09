b41af165b8bd148a9b5d0bb0971a97f6394cd6bf
b125129 <b125129@iiit-bh.ac.in>
2026-08-09 07:37:30 +0530

    Update 6.cpp

--- OOPS_LAB/LAB_2/6.cpp
+++ OOPS_LAB/LAB_2/6.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
 
+// Class definition to manage and convert distance measurements
 class Distance{
+    // Private attributes
     float inches ;
     float feet ;
 
     public:
+      // Member function to take user input for distance components
       void get_data(){
         cout <<"Enter  Inches: " ;
         cin >> inches ;
@@ -13,8 +16,9 @@ class Distance{
         cout <<"Enter feet:";
         cin >> feet ;
       }
-     
+      // Member function to convert total distance to feet and display the result
       void Total_dis(){     
+        // Converts inches to feet (inches / 12) and adds
        cout << "Total distance in feet = "<<  inches/12+ feet  ;
 
       }
@@ -23,6 +27,7 @@ class Distance{
 };
 
 int main(){
+    // Instantiate an object 'd' of class 'Distance'
     Distance d;
     d.get_data();
     d.Total_dis();
